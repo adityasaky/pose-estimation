ad5492ef813428d9dc38bcf4544b3aa754684c89
Aditya Saky <aditya@saky.in>
2018-02-03 07:40:40 +0530

    Add basic pickle load script

---
+++ 2d_transformations/load_all_pickle_files.py
@@ -0,0 +1,8 @@
+import pickle
+import os
+
+
+for f in os.listdir('pickle_files'):
+    if f.endswith('.pkl'):
+        with open("pickle_files/" + f, "r") as file_open:
+            print pickle.load(file_open)
