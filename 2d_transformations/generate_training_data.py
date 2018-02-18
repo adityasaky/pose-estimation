f5ddb053f8392ef38bed04f0c01e910237a5188e
Aditya Saky <aditya@saky.in>
2018-02-18 20:04:44 +0530

    Update to use boundary values

--- 2d_transformations/generate_training_data.py
+++ 2d_transformations/generate_training_data.py
@@ -22,25 +22,29 @@ for pcd_file in os.listdir(source_directory):
                 transformation_name = pkl_file.split('.')[0]
                 target_name = source_name + '_' + transformation_name + '.pcd'
                 print 'Performing ' + transformation_name + '...'
-                with open(target_directory + target_name, 'w') as o:
-                    o.write("# .PCD v0.7 - Point Cloud Data file format\n")
-                    o.write("VERSION 0.7\n")
-                    o.write("FIELDS x y z\n")
-                    o.write("SIZE 4 4 4\n")
-                    o.write("TYPE F F F\n")
-                    o.write("COUNT 1 1 1\n")
-                    o.write("WIDTH " + str(file_width) + "\n")
-                    o.write("HEIGHT 1\n")
-                    o.write("VIEWPOINT 0 0 0 1 0 0 0\n")
-                    o.write("POINTS " + str(file_width) + "\n")
-                    o.write("DATA ascii\n")
-                    for point in all_points:
-                        point_list = point.split(' ')
+                count = 0
+                all_lines = list()
+                for point in all_points:
+                    point_list = point.split(' ')
+                    if -10.0 <= float(point_list[0]) <= 10.0 and -10.0 <= float(point_list[1]) <= 10.0:
+                        count += 1
                         data = np.matrix(point_list[0] + ' ; ' + point_list[1] + ' ; ' + '1')
                         transformation_result = np.dot(transformation, data)
                         x = str(transformation_result[0, 0])
                         y = str(transformation_result[1, 0])
                         z = '0'
                         row = x + ' ' + y + ' ' + z + '\n'
-                        o.write(row)
-
+                        all_lines.append(row)
+            with open(target_directory + target_name, 'w') as o:
+                o.write("# .PCD v0.7 - Point Cloud Data file format\n")
+                o.write("VERSION 0.7\n")
+                o.write("FIELDS x y z\n")
+                o.write("SIZE 4 4 4\n")
+                o.write("TYPE F F F\n")
+                o.write("COUNT 1 1 1\n")
+                o.write("WIDTH " + str(count) + "\n")
+                o.write("HEIGHT 1\n")
+                o.write("VIEWPOINT 0 0 0 1 0 0 0\n")
+                o.write("POINTS " + str(count) + "\n")
+                o.write("DATA ascii\n")
+                o.writelines(all_lines)
