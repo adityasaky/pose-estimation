5bb68f859bb7f036eac0df7b6d5865921cc47e4d
Aditya Saky <aditya@saky.in>
2018-03-03 23:26:46 +0530

    Switch to python pcl and numpy based matrix multiplication - including points filtering

--- 2d_transformations/generate_training_data.py
+++ 2d_transformations/generate_training_data.py
@@ -1,15 +1,42 @@
 import os
 import pickle
 import numpy as np
+import pcl
 
 
 source_directory = "../pointclouds/"
 training_directory = "../training_data/"
 transformations_directory = "./pickle_files/"
 
-x_limit = 10.0
-y_limit = 10.0
+x_limit = 20.0
+y_limit = 20.0
+z_limit = 0.1
 
+for pcd_file in os.listdir(source_directory):
+    source_name = pcd_file.split('.')[0]
+    print "Reading " + source_name
+    all_points = pcl.load(source_directory + pcd_file)
+    all_points_array = all_points.to_array()
+    target_directory = training_directory + source_name + '/'
+    if not os.path.exists(target_directory):
+        os.makedirs(target_directory)
+    print "Filtering points"
+    filtered_points_array = all_points_array[np.where((abs(all_points_array[:, 0]) < x_limit) & (abs(all_points_array[:, 1]) < y_limit) & (all_points_array[:, 2] >= z_limit))]
+    filtered_points_matrix = np.matrix(filtered_points_array)
+    for pkl_file in os.listdir(transformations_directory):
+        with open(transformations_directory + pkl_file, 'r') as t:
+            transformation = pickle.load(t)
+            transformation_name = pkl_file.split('.')[0]
+            target_name = source_name + "-" + transformation_name + ".pcd"
+            print "Performing " + transformation_name
+            transformation_result_matrix = np.dot(filtered_points_matrix, transformation)
+            transformation_result_array = np.array(transformation_result_matrix)
+            transformation_result = pcl.PointCloud()
+            transformation_result.from_array(transformation_result_array)
+            print "Writing " + target_name
+            transformation_result.to_file(target_directory + target_name)
+
+'''
 for pcd_file in os.listdir(source_directory):
     with open(source_directory + pcd_file, 'r') as f:
         all_points = f.readlines()[11:]
@@ -50,3 +77,4 @@ for pcd_file in os.listdir(source_directory):
                 o.write("POINTS " + str(count) + "\n")
                 o.write("DATA ascii\n")
                 o.writelines(all_lines)
+'''
