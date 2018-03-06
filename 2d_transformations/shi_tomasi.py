6db29b6eba663c6869f854b372fd08a0e775c71c
Aditya Saky <aditya@saky.in>
2018-03-06 10:51:50 +0530

    Plot Shi-Tomasi points, rename to use underscore instead of hyphen

--- 2d_transformations/shi_tomasi.py
+++ 2d_transformations/shi_tomasi.py
@@ -10,9 +10,21 @@ source2 = "../pointclouds_filtered/20.0_20.0_0.1/10-20.0_20.0_0.1.jpg"
 img1 = cv2.imread(source1, 0)
 img1 = np.float32(img1)
 corners1 = cv2.goodFeaturesToTrack(img1,1,0.01,10,None,3,0,0.04 )
-print corners1
+corners1 = np.int0(corners1)
+
+for i in corners1:
+    x,y = i.ravel()
+    cv2.circle(img1, (x, y), 3, 255, -1)
+plt.imshow(img1)
+plt.show()
 
 img2 = cv2.imread(source2, 0)
 img2 = np.float32(img2)
 corners2 = cv2.goodFeaturesToTrack(img2,1,0.01,10,None,3,0,0.04)
-print corners2
+corners2 = np.int0(corners2)
+
+for i in corners2:
+    x,y = i.ravel()
+    cv2.circle(img2, (x, y), 3, 255, -1)
+plt.imshow(img2)
+plt.show()
