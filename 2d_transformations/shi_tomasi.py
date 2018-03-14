81c6f55eb67ccadbca39236bb97334875043c077
Anagha Dudihalli <anaghadudihalli@gmail.com>
2018-03-14 15:02:40 +0530

    Update shi tomasi for 30 points

--- 2d_transformations/shi_tomasi.py
+++ 2d_transformations/shi_tomasi.py
@@ -23,7 +23,7 @@ fileList = glob.glob(dir + '*.jpg')
 for jpg_file in fileList:
 	img = cv2.imread(jpg_file, 0)
 	img = np.float32(img)
-	corners = cv2.goodFeaturesToTrack(img,300,0.01,10,None,3,0,0.04)
+	corners = cv2.goodFeaturesToTrack(img,300,0.01,10,None,3,1,0.04)
 	# corners = np.int0(corners)
 	txt_file = jpg_file.split('.jpg')[0] + ".txt"
 	f=open(txt_file,"w")
