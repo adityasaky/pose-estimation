8963d495d43e2b8479d5b5fde67477b970119bbf
Aditya Saky <aditya@saky.in>
2018-03-15 23:49:24 +0530

    Update original shi-tomasi script to write back to pcd

--- 2d_transformations/shi_tomasi.py
+++ 2d_transformations/shi_tomasi.py
@@ -2,8 +2,38 @@ import pcl
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
-import glob
+import os
+import sys
 
+
+height = 160
+width = 160
+heightMeters = 80
+widthMeters = 60
+
+
+directory = "../pointclouds/"
+target_directory = "../pointclouds/test/"
+for source_file in os.listdir(directory):
+    if source_file.endswith('.jpg'):
+        img = cv2.imread(directory + source_file, 0)
+        img = np.float32(img)
+        corners = cv2.goodFeaturesToTrack(img, 300, 0.01, 2, None, 3, 0, 0.04)
+        all_points_array = list()
+        for corner in corners:
+            row = list()
+            x = (corner[0][0] * widthMeters / width) - (widthMeters * 0.5)
+            y = ((height - 1 - corner[0][1]) * heightMeters / height) - (heightMeters * 0.5)
+            row.append(x)
+            row.append(y)
+            row.append(0.0)
+            all_points_array.append(row)
+        all_points = np.array(all_points_array)
+        pcd_file = pcl.PointCloud()
+        pcd_file.from_array(np.float32(all_points))
+        pcd_file.to_file(target_directory + source_file.split('.')[0] + '.pcd')
+
+'''
 source1 = "../pointclouds/2030.jpg"
 source = "2030.jpg"
 img1 = cv2.imread(source1, 0)
@@ -22,7 +52,6 @@ f=open(txt_file,"w")
 f.write(str(corners1))
 f.close()
 
-'''
 for i in corners1:
     x,y = i.ravel()
     cv2.circle(img1, (x, y), 3, 255, -1)
