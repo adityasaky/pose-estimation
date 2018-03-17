a46286362377f3980e19dce475749ff41b54eb8e
Aditya Saky <aditya@saky.in>
2018-03-17 11:09:43 +0530

    Remove filtering in generate script, ignore voxel generations

--- 2d_transformations/generate_training_data.py
+++ 2d_transformations/generate_training_data.py
@@ -5,7 +5,7 @@ import pcl
 import sys
 
 
-source_directory = "../pointclouds_filtered/20.0_20.0_0.1/"
+source_directory = "../pointclouds/"
 training_directory = "../training_data/"
 transformations_directory = "./pickle_files/"
 
@@ -24,16 +24,16 @@ for pcd_file in os.listdir(source_directory):
     if not os.path.exists(target_directory):
         os.makedirs(target_directory)
     print "Filtering points"
-    filtered_points_array = all_points_array[np.where((abs(all_points_array[:, 0]) < x_limit) & (abs(all_points_array[:, 1]) < y_limit) & (all_points_array[:, 2] >= z_limit))]
-    filtered_points_matrix = np.matrix(filtered_points_array)
-    filtered_points_matrix = np.matrix.transpose(filtered_points_matrix)
+    #filtered_points_array = all_points_array[np.where((abs(all_points_array[:, 0]) < x_limit) & (abs(all_points_array[:, 1]) < y_limit) & (all_points_array[:, 2] >= z_limit))]
+    all_points_matrix = np.matrix(all_points_array)
+    all_points_matrix = np.matrix.transpose(all_points_matrix)
     for pkl_file in os.listdir(transformations_directory):
         with open(transformations_directory + pkl_file, 'r') as t:
             transformation = pickle.load(t)
             transformation_name = pkl_file.split('.')[0]
             target_name = source_name + "-" + transformation_name + ".pcd"
             print "Performing " + transformation_name
-            transformation_result_matrix = np.dot(transformation, filtered_points_matrix)
+            transformation_result_matrix = np.dot(transformation, all_points_matrix)
             transformation_result_array = np.array(np.matrix.transpose(transformation_result_matrix))
             transformation_result = pcl.PointCloud()
             transformation_result.from_array(np.float32(transformation_result_array))
