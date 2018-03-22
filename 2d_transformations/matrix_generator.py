f12ec4c297489102688d30f732a35d374f197907
Aditya Saky <aditya@saky.in>
2018-03-22 19:32:44 +0530

    Switch to separate translations along x and y - 756 files

--- 2d_transformations/matrix_generator.py
+++ 2d_transformations/matrix_generator.py
@@ -16,18 +16,19 @@ def rotate(angle):
     return rot_mat
 
 
-def translate(distance):
+def translate(distance_x, distance_y):
     trn_mat = np.identity(3)
-    trn_mat[0][2] = distance
-    trn_mat[1][2] = distance
+    trn_mat[0][2] = distance_x
+    trn_mat[1][2] = distance_y
     return trn_mat
 
 
 for i in range(0, 61, 3):
     for j in range(0, 11, 2):
-        rotation_matrix = rotate(i)
-        translation_matrix = translate(j)
-        transformation_matrix = np.dot(rotation_matrix, translation_matrix)
-        pickle_name = "tm_" + str(i) + "_" + str(j) + ".pkl"
-        with open(os.path.dirname(sys.modules['__main__'].__file__) + "/pickle_files/" + pickle_name, 'wb+') as f:
-            pickle.dump(transformation_matrix, f)
+        for k in range(0, 11, 2):
+            rotation_matrix = rotate(i)
+            translation_matrix = translate(j, k)
+            transformation_matrix = np.dot(rotation_matrix, translation_matrix)
+            pickle_name = "tm_" + str(i) + "_" + str(j) + "_" + str(k) + ".pkl"
+            with open(os.path.dirname(sys.modules['__main__'].__file__) + "/pickle_files/" + pickle_name, 'wb+') as f:
+                pickle.dump(transformation_matrix, f)
