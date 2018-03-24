2d224b92301d0ae8d59b3271b7473ddcdbb4b7ca
Aditya Saky <aditya@saky.in>
2018-03-25 00:10:02 +0530

    Update to support decimals

--- 2d_transformations/matrix_generator.py
+++ 2d_transformations/matrix_generator.py
@@ -5,7 +5,7 @@ import os, sys
 
 
 def rotate(angle):
-    radian = angle * math.pi / 180
+    radian = angle * math.pi / 180.0
     rot_mat = np.identity(3)
     sine = math.sin(radian)
     cosine = math.cos(radian)
@@ -22,13 +22,15 @@ def translate(distance_x, distance_y):
     trn_mat[1][2] = distance_y
     return trn_mat
 
-
-for i in range(0, 61, 3):
+i = 0.0
+#for i in range(0, 61, 3):
+while i < 61.0:
     for j in range(0, 11, 2):
         for k in range(0, 11, 2):
             rotation_matrix = rotate(i)
             translation_matrix = translate(j, k)
             transformation_matrix = np.dot(rotation_matrix, translation_matrix)
-            pickle_name = "tm_" + str(i) + "_" + str(j) + "_" + str(k) + ".pkl"
-            with open(os.path.dirname(sys.modules['__main__'].__file__) + "/pickle_files/" + pickle_name, 'wb+') as f:
+            pickle_name = "tm_" + str(i) + "_" + str(j) + "_" + str(k) + "-.pkl"
+            with open(os.path.dirname(sys.modules['__main__'].__file__) + "/pickle_files_half/" + pickle_name, 'wb+') as f:
                 pickle.dump(transformation_matrix, f)
+    i += 1.5
