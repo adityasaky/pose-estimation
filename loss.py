4cb694dc94db37d779c63887a4c8b902bedc8741
Aditya Saky <aditya@saky.in>
2018-03-31 19:51:09 +0530

    Log call

--- loss.py
+++ loss.py
@@ -5,8 +5,9 @@ import numpy as np
 
 def loss(source):
     def homography_loss(y_true, y_pred):
+        print("HELLO")
+        print(y_true.shape)
         source_pcd = pcl.load(source)
-        y_transformation_true = np.identity(3)
         a = y_pred - y_true
         return a
     return homography_loss
