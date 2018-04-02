ba9912ab49087a7bd779e76c35e32192f770c21b
Aditya Saky <aditya@saky.in>
2018-04-02 20:00:37 +0530

    Add validation data

--- fit_try.py
+++ fit_try.py
@@ -1,5 +1,6 @@
 from posenet.model import create_model
-from keras.optimizers import RMSprop
+from keras.callbacks import History
+from keras.optimizers import RMSprop, Adadelta
 import keras.backend as K
 from tensorflow.python import debug as tf_debug
 from loss import loss_test
@@ -12,7 +13,8 @@ def main():
     sess = tf_debug.LocalCLIDebugWrapperSession(sess)
     K.set_session(sess)'''
     model = create_model()
-
+    history = History()
+    
     base_lr = 0.005
 
     x_train = list()
@@ -27,39 +29,43 @@ def main():
             x_train.append(f['images'][i])
             y_train.append(f['truth'][i])
 
-    '''for npz_file in os.listdir('dataset_eval/all_transformations/'):
+    for npz_file in os.listdir('dataset_eval/all_transformations/'):
         f = np.load('dataset_eval/all_transformations/' + npz_file)
         for i in range(len(f['images'])):
             print(npz_file)
             print(i)
-            x_eval.append(f['images'][i])'''
+            x_eval.append(f['images'][i])
+            y_eval.append(f['truth'][i])
 
     print(len(x_train))
     x_train = np.array(x_train)
     y_train = np.array(y_train)
     x_eval = np.array(x_eval)
+    y_eval = np.array(y_eval)
 
     rms = RMSprop(lr=base_lr, rho=0.9, epsilon=None, decay=0.0)
+    ada = Adadelta(lr=base_lr, rho=0.95, epsilon=None, decay=0.0)
     print("Calling compile")
     #model.compile(optimizer=rms, loss=loss(sample_file),  metrics=['acc'])
-    model.compile(optimizer=rms, loss='mean_squared_error',  metrics=['acc'])
+    model.compile(optimizer=ada, loss='mean_squared_error',  metrics=['acc'])
     print("Compile done")
 
     for layer in model.layers:
         print("Input shape: " + str(layer.input_shape) + ". Output shape: " + str(layer.output_shape))
 
     print("Calling fit")
-    history = model.fit([x_train],
+    model.fit([x_train],
               [y_train],
               batch_size=16,
-              epochs=1,
-              validation_data=None,
-              shuffle=False)
+              epochs=5,
+              validation_data=([x_eval], [y_eval]),
+              shuffle=False,
+              callbacks=[history])
 
 #    predicted = model.predict([x_eval],
 #                  batch_size=22)
 
-    print(history)
+#    print(history.History)
 
 if __name__ == main():
     main()
