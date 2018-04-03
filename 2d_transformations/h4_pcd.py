eeef12b428a4906a9207c3e60e2b746a605d6b7b
Aditya Saky <aditya@saky.in>
2018-04-04 01:35:14 +0530

    Find delta u and delta v

--- 2d_transformations/h4_pcd.py
+++ 2d_transformations/h4_pcd.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import pcl
 import numpy as np
 import pickle
@@ -9,12 +10,23 @@ y_limit = 64.0
 z_limit = -1.0
 source_directory = "../pointclouds/"
 target_directory = "../pointclouds_h4/"
+pickle_directory = "./pickle_files/"
 
 height = 128
 width = 128
 
 height_meters = 64
 width_meters = 64
+
+all_transformations = dict()
+
+def get_transformations():
+    global all_transformations
+    global pickle_directory
+    for pkl_file in os.listdir(pickle_directory):
+        with open(pickle_directory + pkl_file, "rb") as t:
+            transformation = pickle.load(t, encoding='latin1')
+            all_transformations[pkl_file.split('.')[0]] = transformation
 
 
 def scalex(x):
@@ -29,6 +41,8 @@ def scaley(y):
     return ys
 
 
+get_transformations()
+
 for pcd_file in os.listdir(source_directory):
     if not pcd_file.endswith('.pcd'):
         continue
@@ -41,6 +55,20 @@ for pcd_file in os.listdir(source_directory):
     for i in range(len(filtered_points_array)):
         filtered_points_array[i][0] = scalex(filtered_points_array[i][0])
         filtered_points_array[i][1] = scaley(filtered_points_array[i][1])
-    updated_points = pcl.PointCloud()
-    updated_points.from_array(filtered_points_array)
-    updated_points.to_file(target.encode('ASCII'))
+    filtered_points_matrix = np.matrix(filtered_points_array)
+    #updated_points = pcl.PointCloud()
+    #updated_points.from_array(filtered_points_array)
+    #updated_points.to_file(target.encode('ASCII'))
+    p1 = np.array((0, 0, 1))
+    p2 = np.array((0, 128, 1))
+    p3 = np.array((128, 128, 1))
+    p4 = np.array((128, 0, 1))
+    for transformation_label in all_transformations:
+        p1_prime = np.dot(all_transformations[transformation_label], p1)
+        p2_prime = np.dot(all_transformations[transformation_label], p2)
+        p3_prime = np.dot(all_transformations[transformation_label], p3)
+        p4_prime = np.dot(all_transformations[transformation_label], p4)
+        print(p1 - p1_prime)
+        print(p2 - p2_prime)
+        print(p3 - p3_prime)
+        print(p4 - p4_prime)
