e1885c2d03185eed0a5759a50c69826c3dc93284
Ajay <iamajaynaidu@gmail.com>
2018-04-11 18:18:01 +0530

    predict script

--- predict.py
+++ predict.py
@@ -3,19 +3,14 @@ import numpy as np
 import os
 
 
-def predict(batch_size, steps):
-    test_images = list()
+def predict():
     model_path = './results/2018-04-11-...'
-    image_path = ''
+    file_path = ''
     model = load_model(model_path)
-    for npz_file in os.listdir(image_path):
-        f = np.load(image_path + npz_file)
-        for i in range(len(f['images'])):
-            test_images.append(f['images'][i])
-
-    test_images = np.array(test_images)
-    prediction = model.predict([test_images], batch_size=batch_size, verbose=0, steps=steps)
-    return prediction
+    archive = np.load(file_path)
+    test_files = archive['images']
+    prediction = model.predict([test_images])
+    print(prediction)
 
 
-print(predict())
+predict()
