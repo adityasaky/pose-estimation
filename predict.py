e90e0424a3bd96776ab5f1e3bb004989790ebbe1
Aditya Saky <aditya@saky.in>
2018-04-11 11:04:32 +0530

    Modify predict script

--- predict.py
+++ predict.py
@@ -1,14 +1,21 @@
-import keras
+from keras.models import load_model
 import numpy as np
 import os
 
-def predictor(batch_size, steps):
+
+def predict(batch_size, steps):
     test_images = list()
-    
-    for npz_file in os.listdir('dataset_test/h4/'):
-        f = np.load('dataset_test/h4/' + npz_file)
-	for i in range(len(f['images'])):
+    model_path = './results/2018-04-11-...'
+    image_path = ''
+    model = load_model(model_path)
+    for npz_file in os.listdir(image_path):
+        f = np.load(image_path + npz_file)
+        for i in range(len(f['images'])):
             test_images.append(f['images'][i])
 
     test_images = np.array(test_images)
-    prediction = model.predict([test_images],batch_size = batch_size, verbose = 0, steps = steps)
+    prediction = model.predict([test_images], batch_size=batch_size, verbose=0, steps=steps)
+    return prediction
+
+
+print(predict())
