a8889c426241ff0134e298a38550a7923b625446
Aditya Saky <aditya@saky.in>
2018-05-07 08:28:04 +0530

    Remove obsolete scaling lines

--- generate_test_h4.py
+++ generate_test_h4.py
@@ -7,40 +7,22 @@ import numpy as np
 x_limit = 32.0
 y_limit = 32.0
 z_limit = -1.0
-source_directory = "./filtered_new_set/"
-target_directory = "./test_set/"
+source_directory = "/home/saky/tmp/test_data_extra_32/"
+target_directory = "/home/saky/tmp/test_data_pairs/"
 height = 64
 width = 64
-
-height_meters = 32
-width_meters = 32
-
 current = 10
-
-
-def scale_x(x):
-    global width_meters, width
-    xs = int(width * (width_meters * 0.5 + x) / width_meters)
-    return xs
-
-
-def scale_y(y):
-    global height_meters, height
-    ys = int(height * (height_meters * 0.5 + y) / height_meters)
-    return height - 1 - ys
 
 
 def map_to_image(x, y):
     global height, width
-    global height_meters, width_meters
     image = np.zeros((height, width, 3), np.uint8)
     for i in range(len(x)):
-        if (abs(x[i]) <= width_meters * 0.5) & (abs(y[i]) <= height_meters * 0.5):
-            xs = scale_x(x[i])
-            ys = scale_y(y[i])
-            image[ys, xs, 0] = 255
-            image[ys, xs, 1] = 255
-            image[ys, xs, 2] = 255
+        xs = int(x[i])
+        ys = height - 1 - int(y[i])
+        image[ys, xs, 0] = 255
+        image[ys, xs, 1] = 255
+        image[ys, xs, 2] = 255
     return image
 
 
@@ -51,11 +33,12 @@ def create_image(pcd_array):
 
 
 def generate():
+    global current
     while current <= 4310:
         current_file = str(current) + ".pcd"
         next_file = str(current + 10) + ".pcd"
-        i1_points = pcl.load(current_file)
-        i2_points = pcl.load(next_file)
+        i1_points = pcl.load(source_directory + current_file)
+        i2_points = pcl.load(source_directory + next_file)
         if not os.path.exists(target_directory):
             os.makedirs(target_directory)
         i1_points_array = i1_points.to_array()
@@ -65,4 +48,8 @@ def generate():
         result_image = np.dstack((i1, i2))
         target_file_name = str(current) + "_" + str(current + 10)
         print("pairing" + current_file + "&" + next_file + "...")
-        np.save(target_file_name, result_image)
+        np.savez_compressed(target_directory + target_file_name, images=result_image)
+        current += 10
+
+
+generate()
