4c755c7c40e9c6075954a4ed5498d053293afee4
Anagha Dudihalli <anaghadudihalli@gmail.com>
2018-05-08 23:37:05 +0530

    Modify find_left_turn_transformations.py

--- 2d_transformations/find_left_turn_transformations.py
+++ 2d_transformations/find_left_turn_transformations.py
@@ -6,17 +6,17 @@ for f in os.listdir('/home/anagha/fyproject/1'):
     if f.endswith('.pkl'):
         with open("/home/anagha/fyproject/1/" + f, "rb") as file_open:
             file = pickle.load(file_open)
-
-for element in file:
-    #print("---")
-    #print(element)
-    #print("---")
-    #print(file[element])
-    #print("---")
-    #print(file[element][0])
-    #print("---")
-    #print(file[element][0][0])
-    #print("---")
-    angle = math.acos(file[element][0][0])
-    if angle > 0 and angle <= 10.0:
-        print(element)
+            for pair in file:
+                #print("---")
+                #print(pair)
+                #print("---")
+                #print(file[pair])
+                #print("---")
+                #print(file[pair][0])
+                #print("---")
+                #print(file[pair][0][0])
+                #print("---")
+                angle = math.acos(file[pair][0][0])
+                #print(angle)
+                if angle >= 0.0 and angle <= 10.0:
+                    print(pair)
