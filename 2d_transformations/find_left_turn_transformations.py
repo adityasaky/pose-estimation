b4b73b9cf704c69d7aa41e75bcd8f11feda4eb0e
Anagha Dudihalli <anaghadudihalli@gmail.com>
2018-05-09 10:00:09 +0530

    Add count to find_left_turn_transformations.py

--- 2d_transformations/find_left_turn_transformations.py
+++ 2d_transformations/find_left_turn_transformations.py
@@ -2,6 +2,7 @@ import numpy as np
 import csv
 import math
 
+count = 0
 line_no = 0
 delta_x = []
 delta_y = []
@@ -26,6 +27,9 @@ for i in range(0,434):
     delta_yaw_angle = yaw_angle[i+1] - yaw_angle[i]
     if math.degrees(delta_yaw_angle) > 0 and math.degrees(delta_yaw_angle) <= 10.0:
         print("---")
+        count = count + 1
+        print(count)
+        print(math.degrees(delta_yaw_angle))
         print(i)
         print(i+1)
 
