f7a84bc87c28070d2864f799d03727c0cb0af5b3
Aditya Saky <aditya@saky.in>
2018-05-11 16:34:09 +0530

    Rename file for finding specific pairs in ground truth

---
+++ 2d_transformations/find_specific_range_rotation_ground_truth.py
@@ -0,0 +1,33 @@
+import numpy as np
+import os
+import shutil
+
+
+ground_truth_file= "../docs/ground_truth.txt"
+sample_directory = "/home/saky/tmp/test_data_pairs/"
+target_directory = "/home/saky/tmp/test_data_pairs_10_only/"
+ground_truth = open(ground_truth_file, "r").readlines()
+i = 10
+samples = list()
+while i < len(ground_truth):
+    print(i)
+    j = i + 10
+    if j > len(ground_truth):
+        break
+    ground_truth_i = ground_truth[i-1].split(" ")
+    theta_i = float(ground_truth_i[2])
+    ground_truth_j = ground_truth[j-1].split(" ")
+    theta_j = float(ground_truth_j[2])
+    theta_del = np.rad2deg(theta_j - theta_i)
+    if 0.0 <= abs(theta_del) <= 10.0:
+        sample_name = str(i) + "_" + str(j) + ".npz"
+        samples.append(sample_name)
+    i += 10
+
+
+print(samples)
+
+for npz in samples:
+    if os.path.exists(sample_directory + npz):
+        shutil.copy(sample_directory + npz, target_directory + npz)
+
