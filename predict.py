baaed45b8f6c7f0bed74c8b133a6febeb9e7c303
Aditya Saky <aditya@saky.in>
2018-05-13 11:33:14 +0530

    Fix spacing issues

--- predict.py
+++ predict.py
@@ -4,35 +4,16 @@ import os
 
 
 def computeHomographyFromH4P(H4p, img_dims):
-
-    ps = np.array([[0,0],[0,img_dims[1]], [img_dims[0],img_dims[1]],[img_dims[0],0]])
-
+    ps = np.array([[0, 0], [0, img_dims[1]], [img_dims[0], img_dims[1]], [img_dims[0], 0]])
     qs = ps - H4p
-
-
-
-    P = np.zeros((9,9))
-
+    P = np.zeros((9, 9))
     for i in range(4):
-
-        P[2*i] = np.array([-ps[i,0], -ps[i,1], -1, 0, 0, 0, ps[i,0]*qs[i,0], ps[i,1]*qs[i,0], qs[i,0]])
-
-        P[2*i+1] = np.array([0, 0, 0, -ps[i,0], -ps[i,1], -1, ps[i,0]*qs[i,1], ps[i,1]*qs[i,1], qs[i,1]])
-
-    P[-1,-1] = 1
-
-
-
+        P[2 * i] = np.array([-ps[i, 0], -ps[i, 1], -1, 0, 0, 0, ps[i, 0] * qs[i, 0], ps[i,1 ] * qs[i, 0], qs[i, 0]])
+        P[2 * i + 1] = np.array([0, 0, 0, -ps[i, 0], -ps[i, 1], -1, ps[i, 0] * qs[i, 1], ps[i, 1] * qs[i, 1], qs[i, 1]])
+    P[-1, -1] = 1
     b = np.zeros((9))
-
     b[-1] = 1
-
-
-
-    H = np.linalg.solve(P,b)
-
-    
-
+    H = np.linalg.solve(P, b)
     return H
 
 
