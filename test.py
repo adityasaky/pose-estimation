ec0f39e0c3d6c82ba152f3113d2852f53c2d9e64
Aditya Saky <aditya@saky.in>
2018-07-04 22:58:57 +0530

    Add validation data

--- test.py
+++ test.py
@@ -9,9 +9,12 @@ import os
 def main():
     model = create_model()
     base_lr = 0.005
-    epochs = 2
-    batch_size = 8
-    steps_per_epoch = 24715
+    epochs = 25
+    batch_size = 19
+    steps_per_epoch = 9804
+    training_data_directory = ""
+    val_batch_size = 8
+    validation_data_directory = ""
     day = str(datetime.datetime.now()).split('.')[0].split(' ')[0]
     time = '-'.join(str(datetime.datetime.now()).split('.')[0].split(' ')[1].split(':'))
     timestamp = day + '-' + time
@@ -31,12 +34,12 @@ def main():
     checkpoint_path = result_path + "model_checkpoint.h5"
     checkpointer = ModelCheckpoint(filepath=checkpoint_path, monitor='loss', save_weights_only=False, mode='auto')
     print("Calling fit")
-    model.fit_generator(dat_loader('./dataset_h4_full_test_refiled/', batch_size),
+    model.fit_generator(dat_loader(training_data_directory, batch_size),
                                     epochs=epochs,
-                                    validation_data=None,
+                                    validation_data=dat_loader(validation_data_directory, val_batch_size),
                                     steps_per_epoch=steps_per_epoch,
                                     callbacks=[checkpointer],
-                                    shuffle=False)
+                                    shuffle=True)
 
     model.save(result_path + "model.h5")
     model.save_weights(result_path + "model_weights.h5")
